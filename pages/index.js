2948a95ae2b2ea071ef20aeed810b5e325d6e9da
lee7198 <lee7198@naver.com>
2022-07-23 04:02:26 +0900

    feature : group chat detail

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
-import GroupCard from '../components/GroupCard';
+import GroupChatDetail from '../components/GroupChatDetail';
 
 export default function Home() {
   return (
     <div className={'home'}>
-      <GroupCard
-        image={
-          'https://www.gyeongju.go.kr/upload/content/thumb/20200625/D2B1B07FDDAE42639D179009999E5017.jpg'
-        }
-        title={'경주월드... 좋아하시나요!!?'}
-        location={'경상북도 경주시'}
-        date={'2022-07-22'}
-        limit={'6'}
-        current={'4'}
-      />
+      <GroupChatDetail />
     </div>
   );
 }
