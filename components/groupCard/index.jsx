ecdb93058795fbe7c1fb34f598f132d3207d2a75
lee7198 <lee7198@naver.com>
2022-07-23 01:22:29 +0900

    feat : props 수정

--- components/groupCard/index.jsx
+++ components/groupCard/index.jsx
@@ -15,10 +15,7 @@ import {
   CardInfoSubTitle,
 } from '../../src/components/style';
 
-function MainListCard({ image, uid }) {
-  const imgUrl =
-    'https://www.gyeongju.go.kr/upload/content/thumb/20200625/D2B1B07FDDAE42639D179009999E5017.jpg';
-
+function GroupCard({ image, title, location, date, limit, current }) {
   return (
     <>
       <Card
@@ -29,24 +26,26 @@ function MainListCard({ image, uid }) {
       >
         <Row>
           <Col span={14} style={{ padding: '10px 0 10px 10px' }}>
-            <h3 style={CardTitle}>경주월드... 좋아하시나요!!?</h3>
+            <h3 style={CardTitle}>{title}</h3>
             <div style={CardInfoSubTitle}>
               <CompassOutlined />
-              <p style={CardInfoText}>경상북도 경주시</p>
+              <p style={CardInfoText}>{location}</p>
             </div>
             <div style={{ padding: '5px 0 0 0' }}>
               <div style={CardInfo}>
                 <CalendarOutlined />
-                <p style={CardInfoText}>2022.07.22</p>
+                <p style={CardInfoText}>{date}</p>
               </div>
               <div style={CardInfo}>
                 <UserOutlined />
-                <p style={CardInfoText}>모집인원 2 / 8</p>
+                <p style={CardInfoText}>
+                  모집인원 {current} / {limit}
+                </p>
               </div>
             </div>
           </Col>
           <Col span={10}>
-            <img style={CardImage} src={imgUrl} />
+            <img style={CardImage} src={image} />
           </Col>
         </Row>
       </Card>
@@ -54,4 +53,4 @@ function MainListCard({ image, uid }) {
   );
 }
 
-export default MainListCard;
+export default GroupCard;
